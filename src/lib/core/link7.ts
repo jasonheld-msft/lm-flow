62031f2bd59a64532a065c524ab752c9d464c374
Mike Hopcroft <mhopcroft@gmail.com>
2023-10-31 10:54:36 -0700

    WIP: checkpoint link7.ts and link7test.ts

--- src/lib/core/link7.ts
+++ src/lib/core/link7.ts
@@ -9,25 +9,26 @@
  *   2. FIXED: In processSequence(), call to process(models, link.left, input) doesn't
  *      match any overloads.
  ******************************************************************************/
-import {AvailableModels, IAvailableModels, createModel} from './models.js';
+import {IAvailableModels} from './models.js';
 
-type ModelLink<INPUT, OUTPUT, JUDGMENT> = {
+export type ModelLink<INPUT, OUTPUT, JUDGMENT> = {
   type: 'model';
   name: string;
   model: string;
   input: (x: INPUT) => string;
   output: (x: string) => OUTPUT;
+  expected?: OUTPUT;
   judge?: (observed: OUTPUT, expected: OUTPUT) => JUDGMENT;
 };
 
-type SequenceLink<INPUT, OUTPUT, MIDDLE, LEFT, RIGHT> = {
+export type SequenceLink<INPUT, OUTPUT, MIDDLE, LEFT, RIGHT> = {
   type: 'sequence';
   left: AnyLink<INPUT, MIDDLE> & LEFT;
   right: AnyLink<MIDDLE, OUTPUT> & RIGHT;
 };
 
 // The union of types of Links in a tuple.
-type MuxTypes<T> = T extends readonly [
+export type MuxTypes<T> = T extends readonly [
   AnyLink<infer INPUT, infer OUTPUT>,
   ...infer Tail
 ]
@@ -35,29 +36,37 @@ type MuxTypes<T> = T extends readonly [
   : never;
 
 // The union of OUTPUT types of Links in a tuple.
-type MuxOutputUnion<T> = T extends readonly [
+export type MuxOutputUnion<T> = T extends readonly [
   AnyLink<any, infer OUTPUT>,
   ...infer Tail
 ]
   ? OUTPUT | MuxOutputUnion<Tail>
   : never;
 
+export type MuxOutputTypes<T> = T extends  readonly [
+      AnyLink<infer INPUT, infer OUTPUT>,
+      ...infer Tail
+    ]
+  ? ProcessType<AnyLink<INPUT, OUTPUT>> | MuxOutputTypes<Tail>
+  : never;
+  
+  
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type MuxLink<INPUT, OUTPUT, CHILDREN extends AnyLink<any, any>[]> = {
+export type MuxLink<INPUT, OUTPUT, CHILDREN extends AnyLink<any, any>[]> = {
   type: 'mux';
   input: (x: INPUT) => MuxTypes<CHILDREN>[];
   output: (x: MuxOutputUnion<CHILDREN>[]) => OUTPUT;
   children: CHILDREN;
 };
 
-type AnyLink<I, O> = ModelLink<I, O, any> | SequenceLink<I, O, any, any, any> | MuxLink<I, O, any>;
+export type AnyLink<I, O> = ModelLink<I, O, any> | SequenceLink<I, O, any, any, any> | MuxLink<I, O, any>;
 
 ///////////////////////////////////////////////////////////////////////////////
 //
 // Processor
 //
 ///////////////////////////////////////////////////////////////////////////////
-type ProcessType<LINK> = LINK extends ModelLink<any, any, any>
+export type ProcessType<LINK> = LINK extends ModelLink<any, any, any>
   ? ProcessModelType<LINK>
   : LINK extends SequenceLink<any, any, any, any, any>
   ? ProcessSequenceType<LINK>
@@ -75,11 +84,12 @@ type ProcessModelType<LINK> = LINK extends ModelLink<
       prompt: string;
       completion: string;
       output: OUTPUT;
+      expected?: OUTPUT;
       judgment?: JUDGMENT;
     }
   : never;
 
-type ProcessSequenceType<LINK> = LINK extends SequenceLink<
+export type ProcessSequenceType<LINK> = LINK extends SequenceLink<
   infer INPUT,
   infer OUTPUT,
   infer MIDDLE,
@@ -94,62 +104,26 @@ type ProcessSequenceType<LINK> = LINK extends SequenceLink<
     }
   : never;
 
-type ProcessMuxType<LINK> = LINK extends MuxLink<infer INPUT, infer OUTPUT, infer CHILDREN >
+export type ProcessMuxType<LINK> = LINK extends MuxLink<infer INPUT, infer OUTPUT, infer CHILDREN >
   ? Pick<LINK, 'type'> & {
       input: INPUT,
-      // children: 
+      // TODO: reinstate children
+      // pdq: number,
+      // children: MuxOutputTypes<CHILDREN>[],
       output: OUTPUT,
     } 
   : never;
 
-async function processWrapper<INPUT, T extends AnyLink<INPUT,any>>(
+export async function process<INPUT, T extends AnyLink<INPUT,any>>(
   models: IAvailableModels,
   link: T,
   input: INPUT
 ): Promise<ProcessType<T>> {
-  return process(models, link, input) as any;
+  // TODO: remove the following type assertion
+  return processInternal(models, link, input) as any;
 }
 
-// async function processFixAttempt<INPUT, OUTPUT, LINK extends AnyLink<INPUT, OUTPUT>>(
-//   models: IAvailableModels,
-//   link: LINK,
-//   input: INPUT
-// ): Promise<ProcessType<typeof link>> {
-//   const type = link.type;
-//   if (link.type === 'model') {
-//     // Type '{ type: "model"; model: string; name: string; input: INPUT; prompt: string; completion: string; output: OUTPUT; }' is not assignable to type 'ProcessType<LINK>'.
-//     return processModel(models, link, input);
-//   } else if (link.type === 'sequence') {
-//     // Type '{ type: "sequence"; input: INPUT; left: (Pick<ModelLink<INPUT, unknown, any>, "model" | "type" | "name"> & { input: INPUT; prompt: string; completion: string; output: unknown; judgment?: any; }) | (Pick<...> & { ...; }); right: (Pick<...> & { ...; }) | (Pick<...> & { ...; }); output: unknown; }' is not assignable to type 'ProcessType<LINK>'.
-//     return processSequence(models, link, input);
-//   // } else if (link.type === 'mux') {
-//   //   return processMux(models, link, input);
-//   } else {
-//     throw new Error(`Unknown link type "${type}"`);
-//   }
-// }
-
-// type ExtractLink<T> = T extends AnyLink<infer INPUT, infer OUTPUT> ? AnyLink<INPUT, OUTPUT> : never;
-// type ExtractInput<T> = T extends AnyLink<infer INPUT, any> ? INPUT : never;
-
-// async function process<T>(
-//   models: IAvailableModels,
-//   link: ExtractLink<T>,
-//   input: ExtractInput<T>
-// ): Promise<ProcessType<T>> {
-//   const type = link.type;
-//   if (link.type === 'model') {
-//     return processModel(models, link, input);
-//   } else if (link.type === 'sequence') {
-//     return processSequence(models, link, input);
-//   } else if (link.type === 'mux') {
-//     return processMux(models, link, input);
-//   } else {
-//     throw new Error(`Unknown link type "${type}"`);
-//   }
-// }
-
-async function process<INPUT, OUTPUT>(
+export async function processInternal<INPUT, OUTPUT>(
   models: IAvailableModels,
   link: AnyLink<INPUT, OUTPUT>,
   input: INPUT
@@ -171,13 +145,13 @@ async function processModel<INPUT, OUTPUT, JUDGMENT>(
   link: ModelLink<INPUT, OUTPUT, JUDGMENT>,
   input: INPUT
 ) {
-  const {type, model, name} = link;
+  const {type, model, name, judge, expected} = link;
   const prompt = link.input(input);
   const modelAPI = models.getModel({name, defaultModel: model});
   const completion = await modelAPI.complete(prompt);
   const output = link.output(completion);
-  // const judgment = (link.judge && link.expected) ? link.judge(output, link.expected)
-  return {type, model, name, input, prompt, completion, output};
+  const judgment = (judge && expected) ? judge(output, expected) : undefined;
+  return {type, model, name, input, prompt, completion, output, expected, judgment};
 }
 
 async function processSequence<INPUT, OUTPUT, MIDDLE, LEFT, RIGHT>(
@@ -186,12 +160,13 @@ async function processSequence<INPUT, OUTPUT, MIDDLE, LEFT, RIGHT>(
   input: INPUT
 ) {
   const {type} = link;
-  const left = await process(models, link.left, input);
-  const right = await process(models, link.right, left.output);
+  const left = await processInternal(models, link.left, input);
+  const right = await processInternal(models, link.right, left.output);
   return {type, input, left, right, output: right.output};
 }
 
-async function processMux<INPUT, OUTPUT, CHILDREN extends AnyLink<any, any>[]>(
+// TODO: exported for testing
+export async function processMux<INPUT, OUTPUT, CHILDREN extends AnyLink<any, any>[]>(
   models: IAvailableModels,
   link: MuxLink<INPUT, OUTPUT, CHILDREN>,
   input: INPUT
@@ -200,101 +175,7 @@ async function processMux<INPUT, OUTPUT, CHILDREN extends AnyLink<any, any>[]>(
   const promises = link.input(input).map(x => process(models, x.link, x.input));
   const children = (await Promise.all(promises));
   const outputs = children.map(x => x.output);
+  // TODO: remove the following type assertion to any
   const output = link.output(outputs as any);
   return {type, input, children, output};
 }
-
-///////////////////////////////////////////////////////////////////////////////
-//
-// Test cases
-//
-///////////////////////////////////////////////////////////////////////////////
-const models = new AvailableModels([
-  createModel({
-    type: 'mock',
-    name: 'model1',
-    config: {
-      exactMatch: true,
-      defaultResponse: 'error',
-      cache: [
-        {prompt: 'true', completion: 'This statement is true'},
-        {prompt: 'false', completion: 'No way'},
-      ],
-    },
-  }),
-  createModel({
-    type: 'mock',
-    name: 'model2',
-    config: {
-      exactMatch: true,
-      defaultResponse: 'error',
-      cache: [
-        {prompt: '22', completion: 'false'},
-        {prompt: '6', completion: 'true'},
-      ],
-    },
-  }),
-]);
-
-const model1: ModelLink<boolean, number, boolean> = {
-  type: 'model',
-  name: 'model1',
-  model: 'model1',
-  input: (x: boolean) => String(x),
-  output: (x: string) => x.length,
-  judge: (observed: number, expected: number) => observed === expected,
-};
-type M1 = ProcessType<typeof model1>;
-let vm1: M1;
-// vm1.
-export const m1 = await process(models, model1, true);
-
-const model2: ModelLink<number, string, boolean> = {
-  type: 'model',
-  name: 'model2',
-  model: 'model2',
-  input: (x: number) => String(x),
-  output: (x: string) => x,
-  judge: (observed: string, expected: string) => observed === expected,
-};
-export const m2 = await process(models, model1, true);
-
-export const sequence1: SequenceLink<
-  boolean,
-  string,
-  number,
-  typeof model1,
-  typeof model2
-> = {
-  type: 'sequence',
-  left: model1,
-  right: model2,
-};
-type S1 = ProcessType<typeof sequence1>;
-let vs1: S1;
-let vs2: ProcessSequenceType<typeof sequence1>;
-
-export const s = await process(models, sequence1, true);
-export const s1 = await processWrapper(models, sequence1, true);
-console.log(JSON.stringify(s1, null, 2));
-export const s2 = await process(models, sequence1, false);
-console.log(JSON.stringify(s2, null, 2));
-
-export const mux1: MuxLink<number, string, [typeof model1, typeof model2]> = {
-  type: 'mux',
-  input: (x: number) => [
-    {input: true, link: model1},
-    {input: 5, link: model2},
-    {input: false, link: model1},
-  ],
-  output: (x: (number | string)[]) => x.map(y => typeof y).join(', '),
-  children: [model1, model2],
-}
-type MT = MuxTypes<typeof mux1.children>;
-type MuxHead<T> = T extends readonly [AnyLink<any, infer OUTPUT>, ... infer TAIL] ? OUTPUT : never;
-type MH = MuxHead<typeof mux1.children>
-type MU = MuxOutputUnion<typeof mux1.children>
-//type MUX1 = processmux
-// const vmux1 = await processMux(models, mux1, 3);
-const vmux2 = await process(models, mux1, 3);
-console.log(JSON.stringify(vmux2, null, 2));
---
+++ src/lib/core/link7test.ts
@@ -0,0 +1,103 @@
+import { AnyLink, ModelLink, MuxLink, MuxOutputUnion, MuxTypes, ProcessSequenceType, ProcessType, SequenceLink, processInternal, process, processMux } from './link7.js';
+import {AvailableModels, createModel} from './models.js';
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Test cases
+//
+///////////////////////////////////////////////////////////////////////////////
+const models = new AvailableModels([
+  createModel({
+    type: 'mock',
+    name: 'model1',
+    config: {
+      exactMatch: true,
+      defaultResponse: 'error',
+      cache: [
+        {prompt: 'true', completion: 'This statement is true'},
+        {prompt: 'false', completion: 'No way'},
+      ],
+    },
+  }),
+  createModel({
+    type: 'mock',
+    name: 'model2',
+    config: {
+      exactMatch: true,
+      defaultResponse: 'error',
+      cache: [
+        {prompt: '22', completion: 'false'},
+        {prompt: '6', completion: 'true'},
+      ],
+    },
+  }),
+]);
+
+const model1: ModelLink<boolean, number, boolean> = {
+  type: 'model',
+  name: 'model1',
+  model: 'model1',
+  input: (x: boolean) => String(x),
+  output: (x: string) => x.length,
+  judge: (observed: number, expected: number) => observed === expected,
+};
+// type M1 = ProcessType<typeof model1>;
+// let vm1: M1;
+// vm1.
+export const m1 = await processInternal(models, model1, true);
+
+const model2: ModelLink<number, string, boolean> = {
+  type: 'model',
+  name: 'model2',
+  model: 'model2',
+  input: (x: number) => String(x),
+  output: (x: string) => x,
+  judge: (observed: string, expected: string) => observed === expected,
+};
+export const m2 = await processInternal(models, model1, true);
+
+export const sequence1: SequenceLink<
+  boolean,
+  string,
+  number,
+  typeof model1,
+  typeof model2
+> = {
+  type: 'sequence',
+  left: model1,
+  right: model2,
+};
+type S1 = ProcessType<typeof sequence1>;
+let vs1: S1;
+let vs2: ProcessSequenceType<typeof sequence1>;
+
+export const s = await processInternal(models, sequence1, true);
+export const s1 = await process(models, sequence1, true);
+console.log(JSON.stringify(s1, null, 2));
+export const s2 = await processInternal(models, sequence1, false);
+console.log(JSON.stringify(s2, null, 2));
+
+export const mux1: MuxLink<number, string, [typeof model1, typeof model2]> = {
+  type: 'mux',
+  input: (x: number) => [
+    {input: true, link: model1},
+    {input: 5, link: model2},
+    {input: false, link: model1},
+  ],
+  output: (x: (number | string)[]) => x.map(y => typeof y).join(', '),
+  children: [model1, model2],
+}
+type MT = MuxTypes<typeof mux1.children>;
+type MuxHead<T> = T extends readonly [AnyLink<any, infer OUTPUT>, ... infer TAIL] ? OUTPUT : never;
+type MH = MuxHead<typeof mux1.children>
+type MU = MuxOutputUnion<typeof mux1.children>
+//type MUX1 = processmux
+const vmux1 = await processMux(models, mux1, 3);
+vmux1;
+vmux1.children[0];
+console.log('=========================');
+console.log(JSON.stringify(vmux1, null, 2));
+const vmux2 = await process(models, mux1, 3);
+// Following does not work yet.
+// vmux2.children
+console.log(JSON.stringify(vmux2, null, 2));
